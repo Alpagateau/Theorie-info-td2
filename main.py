511815e6d8bcc32805d7dee73dfce98a3a072555
Alpa <52045495+Alpagateau@users.noreply.github.com>
2025-12-17 14:25:26 +0100

    first commmit

---
+++ main.py
@@ -0,0 +1,58 @@
+class Node:
+    def __init__(self, l = None, r = None, i = 0, p = 0):
+        self.l : Node = l
+        self.r : Node = r
+        self.i : Int = i
+        self.p : Float = p
+        self.symbol = None
+    
+    def __str__(self):
+        return "( "+str(self.p)+ "("+str(self.i)+"): " +str(self.l) + " | " + str(self.r) +" )"
+
+def huffman_tree(p):
+    trees = [Node(None, None, 0, pro) for pro in p]
+    while len(trees) > 1:
+        trees.sort(reverse = True, key = lambda x : x.p)
+        t1 = trees[-1]
+        t2 = trees[-2]
+        trees = trees[:-2] + [Node(t2, t1, 0, t2.p + t1.p)]
+    t = trees[0]
+    tag_tree(t, 0)
+    return t
+
+def get_cwd(tree):
+    if tree == None: 
+        return [""]
+    if tree.l == None and tree.r == None:
+        return [""]
+    return ["0" + s for s in get_cwd(tree.l)] +["1" + s for s in get_cwd(tree.r)]
+
+def tag_tree(tree, acc, symbols = []):
+    if tree.l == None and tree.r == None:
+        tree.i = acc
+        if symbols != []:
+            tree.symbol = symbols[acc]
+        return acc+1
+    
+    acc = tag_tree(tree.l, acc)
+    acc = tag_tree(tree.r, acc)
+    return acc
+
+def huffman_code(p):
+    tree = huffman_tree(p) 
+    words = get_cwd(tree)
+    return (words, sum([len(w)/len(words) for w in words]))
+
+def huffman_tree2(cwd):
+    if len(cwd) == 0:
+        return Node(None, None, 0, 0)
+    cwdl = [i[1:] for i in cwd if len(i) > 1 and i[0] == "0"]
+    cwdr = [i[1:] for i in cwd if len(i) > 1 and i[0] == "1"] 
+    return Node(huffman_tree2(cwdl), huffman_tree2(cwdr), 0, 0)
+
+a = huffman_tree([0.1, 0.1, 0.15, 0.16, 2])
+cdw = get_cwd(a)
+print(a)
+print(huffman_code([0.1, 0.1, 0.15, 0.16, 2]))
+b = huffman_tree2(cdw)
+print(b)
